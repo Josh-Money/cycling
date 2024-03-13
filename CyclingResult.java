f0c78b851af9d87e7b76b871cbc0420312654d3f
Josh-Money <160910461+Josh-Money@users.noreply.github.com>
2024-03-13 15:15:03 +0000

    6

--- CyclingResult.java
+++ CyclingResult.java
@@ -78,7 +78,7 @@ public class CyclingResult {
     }
 
     public LocalTime[] getCheckpointTimes() {
-        return Arrays.copyOf(checkpointTimes, checkpointTimes.length);
+        return Arrays.copyOf(checkpointTimes, checkpointTimes.size());
     }
 
     public void addCheckpointTimes(LocalTime... times) {
